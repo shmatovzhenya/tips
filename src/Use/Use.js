968e52fd138ce7275ab77f44e0a03075c06186bb
shmatovzhenya <shmatovzhenya@yandex.ru>
2019-10-09 08:48:42 +0300

    Added nested use object feature.

--- src/Use/Use.js
+++ src/Use/Use.js
@@ -1,40 +1,82 @@
 import isEmptyObject from '../utils/isEmptyObject';
 
+
+const compute = async ({ method, options, update, computed }, result = {}) => {
+  let data = {};
+
+  if (!computed || isEmptyObject(computed)) {
+    data = await method(options);
+  }
+
+  if (typeof computed === 'string') {
+    const computedValue = result[computed];
+
+    data = await method(computedValue);
+  }
+
+  if (!isEmptyObject(update)) {
+    for (let [key, useInstance] of Object.entries(update)) {
+      data[key] = await compute(useInstance, {...result, ...data});
+    }
+  }
+
+  return data;
+};
+
+/**
+ * Data transfer object for Data Mapper.
+ */
 class Use {
-  constructor({ method, options, update, fallback }) {
+  #method = undefined;
+  #options = undefined;
+  #update = undefined;
+  #fallback = undefined;
+  #computed = undefined;
+
+  constructor({ method, options, update, fallback, computed }) {
     if (typeof method !== 'function') {
       throw new Error('Method parameter must be a function');
     }
 
-    Object.defineProperty(this, 'method', {
-      enumerable: false,
-      writable: false,
-      get: () => method,
-    });
+    this.#method = method;
 
     if (!isEmptyObject(options)) {
-      Object.defineProperty(this, 'options', {
-        enumerable: false,
-        writable: false,
-        get: () => options,
-      });
+      this.#options = options;
     }
 
     if (!isEmptyObject(update)) {
-      Object.defineProperty(this, 'update', {
-        enumerable: false,
-        writable: false,
-        get: () => update,
-      });
+      this.#update = update;
     }
 
     if (!isEmptyObject(fallback)) {
-      Object.defineProperty(this, 'fallback', {
-        enumerable: false,
-        writable: false,
-        get: () => fallback,
-      });
+      this.#fallback = fallback;
     }
+
+    Object.freeze(this);
+  }
+
+  get method() {
+    return this.#method;
+  }
+
+  get options() {
+    return this.#options;
+  }
+
+  get update() {
+    return this.#update;
+  }
+
+  get fallback() {
+    return this.#fallback;
+  }
+
+  get computed() {
+    return this.#computed;
+  }
+
+  async getResult() {
+    return await compute(this);
   }
 }
 
---
+++ src/Use/Use.test.js
@@ -0,0 +1,63 @@
+import test from 'ava';
+
+import Use from './Use';
+
+
+test('Correct creating use object', t => {
+  const use = new Use({
+    method: async () => {},
+    options: [],
+    update: { a: 1 },
+  });
+
+  t.deepEqual(use.options, undefined);
+  t.deepEqual(use.update, { a: 1 });
+});
+
+test('Testing most trivial case', async (t) => {
+  const use = new Use({
+    method: ({ a, b }) => Promise.resolve({ a: b, b: a }),
+    options: { a: 2, b: 3 },
+  });
+
+  const result = await use.getResult();
+
+  t.deepEqual({ a: 3, b: 2 }, result);
+});
+
+test('Testing correct update data in nesting instances', async (t) => {
+  const nestedUse = new Use({
+    method: () => Promise.resolve(42),
+  });
+
+  const use = new Use({
+    method: ({ a, b }) => Promise.resolve({ a: b, b: a }),
+    options: { a: 2, b: 3 },
+    update: {
+      b: nestedUse,
+    },
+  });
+
+  const result = await use.getResult();
+
+  t.deepEqual({ a: 3, b: 42 }, result);
+});
+
+// test('Testing use previous computed value', async (t) => {
+//   const nestedUse = new Use({
+//     method: a => Promise.resolve(-a),
+//     computed: 'b',
+//   });
+
+//   const use = new Use({
+//     method: ({ a, b }) => Promise.resolve({ a: b, b: a }),
+//     options: { a: 2, b: 3 },
+//     update: {
+//       b: nestedUse,
+//     },
+//   });
+
+//   const result = await use.getResult();
+
+//   t.deepEqual({ a: 3, b: -2 }, result);
+// });
